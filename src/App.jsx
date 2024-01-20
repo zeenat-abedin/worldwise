02f1b3a2f02676506459a273a33303c44f0ef83d
zeenat-abedin <zeenu.23@gmail.com>
2024-01-20 19:03:04 +0530

    Added routes for Product and Pricing pages

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,15 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Home from "./pages/Home";
+import Product from "./pages/Product";
+import Pricing from "./pages/Pricing";
 
 export default function App() {
   return (
     <BrowserRouter>
       <Routes>
         <Route path="/" element={<Home />} />
+        <Route path="/product" element={<Product />} />
+        <Route path="/pricing" element={<Pricing />} />
       </Routes>
     </BrowserRouter>
   );
