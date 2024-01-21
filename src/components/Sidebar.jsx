01849d843789da7fdc03d5f1dea9aaefd02b8a64
zeenat-abedin <zeenu.23@gmail.com>
2024-01-22 00:03:36 +0530

    Add logo to sidebar

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -1,5 +1,10 @@
+import Logo from "./Logo";
 import styles from "./Sidebar.module.css";
 
 export default function Sidebar() {
-  return <div className={styles.sidebar}>Sidebar</div>;
+  return (
+    <div className={styles.sidebar}>
+      <Logo />
+    </div>
+  );
 }
