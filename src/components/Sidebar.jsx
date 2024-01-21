305e133fedb0e3a41b46fa49941a7a5f96e12d90
zeenat-abedin <zeenu.23@gmail.com>
2024-01-22 00:12:13 +0530

    Add AppNav component to the sidebar

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -1,10 +1,14 @@
 import Logo from "./Logo";
+import AppNav from "./AppNav";
 import styles from "./Sidebar.module.css";
 
 export default function Sidebar() {
   return (
     <div className={styles.sidebar}>
       <Logo />
+      <AppNav />
+
+      <p>List of cities</p>
     </div>
   );
 }
