064f0d60af07f55cdf40bfb85456860413d1bf89
zeenat-abedin <zeenu.23@gmail.com>
2024-01-23 21:56:26 +0530

    Add message when there are no cities to display

--- src/components/CityList.jsx
+++ src/components/CityList.jsx
@@ -4,9 +4,13 @@ import CityItem from "./CityItem";
 import Spinner from "./Spinner";
 
 import styles from "./CityList.module.css";
+import Message from "./Message";
 
 export default function CityList({ cities, loading }) {
   if (loading) return <Spinner />;
+  if (!cities.length) {
+    return <Message message="Add your first city by clicking on the map" />;
+  }
 
   return (
     <ul className={styles.CityList}>
