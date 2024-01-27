9b90fbe2f0b4d0abcb2e29c642716dfd2a49e391
zeenat-abedin <zeenu.23@gmail.com>
2024-01-27 22:20:07 +0530

    Add a redirect from `/app` to `/app/cities`

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import { useState, useEffect } from "react";
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
 import HomePage from "./pages/HomePage";
 import Product from "./pages/Product";
 import Pricing from "./pages/Pricing";
@@ -42,6 +42,7 @@ export default function App() {
         <Route path="pricing" element={<Pricing />} />
         <Route path="login" element={<Login />} />
         <Route path="app" element={<AppLayout />}>
+          <Route index element={<Navigate replace to="cities" />} />
           <Route
             index
             element={<CityList cities={cities} loading={loading} />}
