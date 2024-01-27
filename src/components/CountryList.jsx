ef2436122e32a1d6da55cf246ae9ff19817c03b8
zeenat-abedin <zeenu.23@gmail.com>
2024-01-27 13:12:01 +0530

    Reduce cities to countries array

--- src/components/CountryList.jsx
+++ src/components/CountryList.jsx
@@ -7,12 +7,18 @@ import styles from "./CountryList.module.css";
 import Message from "./Message";
 
 export default function CountryList({ cities, loading }) {
-  const countries = [];
-
   if (loading) return <Spinner />;
   if (!cities.length) {
     return <Message message="Add your first country by clicking on the map" />;
   }
+
+  //checking if the array already contains the current city. So we can say the array, then we map over it, and then el.city, which will basically create an array of all the cities that are already in the countries array. And then we ask if this includes the current city. Or actually the country in the current city. Okay, so if that is not the case, so if the current country is not yet in this array that we are creating here, well, then let's return a new array which will contain all the current elements plus a new one.
+
+  const countries = cities.reduce((arr, city) => {
+    if (!arr.map((el) => el.city).includes(city.country)) {
+      return [...arr, { country: city.country, emoji: city.emoji }];
+    } else return arr;
+  }, []);
 
   return (
     <ul className={styles.countryList}>
