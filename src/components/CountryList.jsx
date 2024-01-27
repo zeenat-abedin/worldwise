59d2934cb36b4e1831dab5fdb072b2f33b13c556
zeenat-abedin <zeenu.23@gmail.com>
2024-01-27 15:47:20 +0530

    Update `CountryList` component to use `country` instead of `city` when checking for duplicates.

--- src/components/CountryList.jsx
+++ src/components/CountryList.jsx
@@ -15,7 +15,7 @@ export default function CountryList({ cities, loading }) {
   //checking if the array already contains the current city. So we can say the array, then we map over it, and then el.city, which will basically create an array of all the cities that are already in the countries array. And then we ask if this includes the current city. Or actually the country in the current city. Okay, so if that is not the case, so if the current country is not yet in this array that we are creating here, well, then let's return a new array which will contain all the current elements plus a new one.
 
   const countries = cities.reduce((arr, city) => {
-    if (!arr.map((el) => el.city).includes(city.country)) {
+    if (!arr.map((el) => el.country).includes(city.country)) {
       return [...arr, { country: city.country, emoji: city.emoji }];
     } else return arr;
   }, []);
