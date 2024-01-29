9fb30d48fac837aff5b825bd3ed9e03f5335fa90
zeenat-abedin <zeenu.23@gmail.com>
2024-01-29 18:07:56 +0530

    Update CitiesContext error message

--- src/contexts/CitiesContext.jsx
+++ src/contexts/CitiesContext.jsx
@@ -36,8 +36,8 @@ function CitiesProvider({ children }) {
 
 function useCities() {
   const context = useContext(CitiesContext);
-  if (context === undefined) throw new Error("");
-
+  if (context === undefined)
+    throw new Error("CitiesContext was used outside the CitiesProvider");
   return context;
 }
 
