928e0dde7d08cd864424c46c3e52c9f5126db8b2
zeenat-abedin <zeenu.23@gmail.com>
2024-01-30 16:51:49 +0530

    Make BackButton a separate component

--- src/components/Form.jsx
+++ src/components/Form.jsx
@@ -1,9 +1,9 @@
 // "https://api.bigdatacloud.net/data/reverse-geocode-client?latitude=0&longitude=0"
 
 import { useState } from "react";
-import { useNavigate } from "react-router-dom";
 
 import Button from "./Button";
+import BackButton from "./BackButton";
 import styles from "./Form.module.css";
 
 export function convertToEmoji(countryCode) {
@@ -20,7 +20,6 @@ function Form() {
   const [date, setDate] = useState(new Date());
   const [notes, setNotes] = useState("");
 
-  const navigate = useNavigate();
   return (
     <form className={styles.form}>
       <div className={styles.row}>
@@ -53,15 +52,7 @@ function Form() {
 
       <div className={styles.buttons}>
         <Button type="primary">Add</Button>
-        <Button
-          type="back"
-          onClick={(e) => {
-            e.preventDefault();
-            navigate(-1);
-          }}
-        >
-          &larr; Back
-        </Button>
+        <BackButton />
       </div>
     </form>
   );
