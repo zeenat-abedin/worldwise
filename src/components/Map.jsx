18ea5cc6c98059b31ffb9a80ea581ee4547d6e12
zeenat-abedin <zeenu.23@gmail.com>
2024-01-31 17:59:54 +0530

    Add a check for geolocationPosition before rendering the button to get the user's position

--- src/components/Map.jsx
+++ src/components/Map.jsx
@@ -43,9 +43,12 @@ export default function Map() {
 
   return (
     <div className={styles.mapContainer}>
-      <Button type="position" onClick={getPosition}>
-        {isLoadingPosition ? "Loading..." : "Use your position"}
-      </Button>
+      {!geolocationPosition && (
+        <Button type="position" onClick={getPosition}>
+          {isLoadingPosition ? "Loading..." : "Use your position"}
+        </Button>
+      )}
+
       <MapContainer
         center={mapPosition}
         zoom={6}
