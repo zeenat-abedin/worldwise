dd999cdefaa6e7503ddcd8b31734a9f07aafcf66
zeenat-abedin <zeenu.23@gmail.com>
2024-01-31 22:50:31 +0530

    Add message when no lat/lng

--- src/components/Form.jsx
+++ src/components/Form.jsx
@@ -60,6 +60,8 @@ function Form() {
   }, [lat, lng]);
 
   if (isLoadingGeocoding) return <Spinner />;
+  if (!lat && !lng)
+    return <Message message="Start by clicking somewhere in the map" />;
   if (geoCodingError) return <Message message={geoCodingError} />;
 
   return (
