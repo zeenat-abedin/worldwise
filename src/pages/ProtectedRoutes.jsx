c14d722395041a0056690b5c2bd6515c3247298b
zeenat-abedin <zeenu.23@gmail.com>
2024-02-03 16:04:23 +0530

    Add authentication check for protected routes

--- src/pages/ProtectedRoutes.jsx
+++ src/pages/ProtectedRoutes.jsx
@@ -1,4 +1,15 @@
+import { useEffect } from "react";
+import { useAuth } from "../contexts/FakeAuthContext";
+import { useNavigate } from "react-router-dom";
+
 function ProtectedRoutes({ children }) {
+  const { isAuthenticated } = useAuth();
+  const navigate = useNavigate();
+
+  useEffect(() => {
+    if (!isAuthenticated) navigate("/");
+  }, [isAuthenticated, navigate]);
+
   return children;
 }
 
