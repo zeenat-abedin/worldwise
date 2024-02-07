02f08aaf506fb246964057e8476599b62afda33d
zeenat-abedin <zeenu.23@gmail.com>
2024-02-07 21:13:47 +0530

    Add Suspense component to App.jsx for lazy-loading routes

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import { lazy } from "react";
+import { Suspense, lazy } from "react";
 import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
 
 // import HomePage from "./pages/HomePage";
@@ -16,6 +16,7 @@ import CityList from "./components/CityList";
 import CountryList from "./components/CountryList";
 import City from "./components/City";
 import Form from "./components/Form";
+import SpinnerFullPage from "./components/SpinnerFullPage";
 
 const HomePage = lazy(() => import("./pages/HomePage"));
 const Product = lazy(() => import("./pages/Product"));
@@ -30,6 +31,7 @@ export default function App() {
     <AuthProvider>
       <CitiesProvider>
         <BrowserRouter>
+          <Suspense fallback={<SpinnerFullPage />}></Suspense>
           <Routes>
             <Route index element={<HomePage />} />
             <Route path="product" element={<Product />} />
